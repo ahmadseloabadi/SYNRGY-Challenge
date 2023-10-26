263d31c42c259590fa8de28fe6e256be7e8ae60b
ahmadseloabadi <ahmadseloabadi@gmail.com>
2023-10-27 03:30:03 +0700

    add route css js images

--- server/index.js
+++ server/index.js
@@ -4,37 +4,73 @@
  * HTTP Server
  * */
 const http = require("http");
-const { PORT = 7000 } = process.env; // Ambil port dari environment variable
+const { PORT = 8000 } = process.env; // Ambil port dari environment variable
 
 const fs = require("fs");
 const path = require("path");
 const PUBLIC_DIRECTORY = path.join(__dirname, "../public"); //untuk mengakses folder public
-console.log("dir :", PUBLIC_DIRECTORY);
-
-//membuat function getHTML
-function getHTML(filename) {
-  // Membuat path ke html file yang ada di directory public
-  const htmlFile = path.join(PUBLIC_DIRECTORY, filename);
-
-  // Membaca file HTML pada path htmlFile
-  return fs.readFileSync(htmlFile, "utf-8");
+//membuat function file html
+function getHTML(htmlFileName) {
+  const htmlFilePath = path.join(PUBLIC_DIRECTORY, htmlFileName);
+  return fs.readFileSync(htmlFilePath, "UTF-8");
+}
+//membuat function file css
+function getCSS(cssFileName) {
+  const cssFilePath = path.join(PUBLIC_DIRECTORY, cssFileName);
+  return fs.readFileSync(cssFilePath, "UTF-8");
+}
+//membuat function file js
+function getJS(jsFileName) {
+  const jsFilePath = path.join(PUBLIC_DIRECTORY, jsFileName);
+  return fs.readFileSync(jsFilePath, "UTF-8");
+}
+//membuat function file image
+function getImage(imageFileName) {
+  const imageFilePath = path.join(PUBLIC_DIRECTORY, imageFileName);
+  return fs.readFileSync(imageFilePath);
 }
 
-// Request handler
-// Fungsi yang berjalan ketika ada request yang masuk.
 function onRequest(req, res) {
+  console.log(req.url);
+  let CSS = "";
+  let JS = "";
+  let IMG = "";
+
+  if (req.url.match(".css$")) {
+    CSS = req.url;
+  } else if (req.url.match(".js$")) {
+    JS = req.url;
+  } else if (req.url.match(".png$") || req.url.match(".jpg$")) {
+    IMG = req.url;
+  }
+
   switch (req.url) {
     case "/":
-      // Set header agar client tau kalo response kita itu berupa HTML
-      res.setHeader("Content-Type", "text/html");
-      // Set response status, agar client kita tau kalo requestnya berhasil
-      res.writeHead(200);
-      // Set response body, agar dapat dibaca oleh client
+      res.writeHead(200, { "Content-Type": "text/html" });
       res.end(getHTML("index.html"));
-      break;
+      return;
+    case "/cars":
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(getHTML("cari-mobil.html"));
+      return;
+
+    case CSS:
+      res.writeHead(200, { "Content-Type": "text/css" });
+      res.end(getCSS(CSS));
+      return;
+
+    case JS:
+      res.writeHead(200, { "Content-Type": "application/javascript" });
+      res.end(getJS(JS));
+      return;
+
+    case IMG:
+      res.writeHead(200, { "Content-Type": "image" });
+      res.end(getImage(IMG));
+      return;
+
     default:
-      res.setHeader("Content-Type", "text/html");
-      res.writeHead(200);
+      res.writeHead(404);
       res.end(getHTML("404.html"));
       break;
   }
@@ -43,6 +79,6 @@ function onRequest(req, res) {
 const server = http.createServer(onRequest);
 
 // Jalankan server
-server.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server sudah berjalan, silahkan buka http://0.0.0.0:${PORT}`);
+server.listen(PORT, "", () => {
+  console.log(`Server sudah berjalan, silahkan buka http://localhost:${PORT}`);
 });
