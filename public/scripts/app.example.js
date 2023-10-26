3559a77a7cd762c083e8a9c67178f55217b973fa
ahmadseloabadi <ahmadseloabadi@gmail.com>
2023-10-27 03:28:36 +0700

    update run() to search

--- public/scripts/app.example.js
+++ public/scripts/app.example.js
@@ -1,23 +1,34 @@
 class App {
   constructor() {
+    this.filterByDriver = document.getElementById("driver");
+    this.filterByDate = document.getElementById("date");
+    this.filterByTime = document.getElementById("time");
+    this.filterByCapacity = document.getElementById("passanger");
     this.clearButton = document.getElementById("clear-btn");
-    this.loadButton = document.getElementById("load-btn");
+    this.searchButton = document.getElementById("search-btn");
     this.carContainerElement = document.getElementById("cars-container");
   }
 
   async init() {
     await this.load();
-
     // Register click listener
     this.clearButton.onclick = this.clear;
-    this.loadButton.onclick = this.run;
+    this.searchButton.onclick = this.run;
   }
 
   run = () => {
+    this.clear();
     Car.list.forEach((car) => {
-      const node = document.createElement("div");
-      node.innerHTML = car.render();
-      this.carContainerElement.appendChild(node);
+      let dateTime = this.filterByDate.value + "T" + this.filterByTime.value;
+      let date = Date.parse(dateTime);
+      let time = Date.parse(car.availableAt);
+      let passanger = this.filterByCapacity.value;
+
+      if (time >= date && car.capacity >= passanger) {
+        const node = document.createElement("div");
+        node.innerHTML = car.render();
+        this.carContainerElement.appendChild(node);
+      }
     });
   };
 
